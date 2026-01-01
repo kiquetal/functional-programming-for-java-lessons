b00a1be46b16a806c478f679e61fd35cbace6c52
kiquetal <kiquetal@gmail.com>
2026-01-01 12:04:16 -0300

    add methods to negate criteria for Car objects

--- java-examples/functional-programming/src/main/java/com/functional/java/App.java
+++ java-examples/functional-programming/src/main/java/com/functional/java/App.java
@@ -21,7 +21,54 @@ public class App
 
         System.out.println(getAllCriterion(colors,  s -> s.equals("BLUE")));
 
+        // Test showAll method with cars
+        System.out.println("\n=== Testing showAll method ===");
+        List<Car> cars = createTestCars();
 
+        System.out.println("\nRed cars:");
+        showAll(cars, getColorCriterion("RED"));
+
+        System.out.println("\nRed or Green cars:");
+        showAll(cars, getColorCriterion("RED", "GREEN"));
+
+        System.out.println("\nNot Red cars:");
+        showAll(cars, getInverse(getColorCriterion("RED")));
+
+        System.out.println("\nToyota cars:");
+        showAll(cars, c -> "Toyota".equals(c.getBrand()));
+
+    }
+
+    private static List<Car> createTestCars() {
+        List<Car> cars = new ArrayList<>();
+
+        Car car1 = new Car();
+        car1.setColor("RED");
+        car1.setBrand("Toyota");
+
+        Car car2 = new Car();
+        car2.setColor("BLUE");
+        car2.setBrand("Honda");
+
+        Car car3 = new Car();
+        car3.setColor("GREEN");
+        car3.setBrand("Ford");
+
+        Car car4 = new Car();
+        car4.setColor("RED");
+        car4.setBrand("BMW");
+
+        Car car5 = new Car();
+        car5.setColor("WHITE");
+        car5.setBrand("Toyota");
+
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        cars.add(car4);
+        cars.add(car5);
+
+        return cars;
     }
 
     static void executeCriteria(CarCriteria n) {
@@ -58,6 +105,15 @@ public class App
         return e -> !crit.test(e);
 
     }
+
+    public static <E> void showAll(List<E> list, Criterion<E> criterion) {
+        for (E element : list) {
+            if (criterion.test(element)) {
+                System.out.println(element);
+            }
+        }
+    }
+
     public class CarScratch {
         public static <E> Criterion<E> negate(Criterion<E> crit) {
             return  x -> !crit.test(x);
@@ -103,5 +159,10 @@ class Car {
     }
 
     private String brand;
+
+    @Override
+    public String toString() {
+        return "Car{color='" + color + "', brand='" + brand + "'}";
+    }
 
 }

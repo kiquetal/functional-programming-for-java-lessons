8c7155c17acbda68cc0b07935b0e2322adf6b2ed
kiquetal <kiquetal@gmail.com>
2026-01-05 08:28:00 -0300

    refactor: improve gas level comparison and update lesson4 documentation

--- java-examples/functional-programming/src/main/java/com/functional/java/SuperIterable.java
+++ java-examples/functional-programming/src/main/java/com/functional/java/SuperIterable.java
@@ -3,6 +3,7 @@ package com.functional.java;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class SuperIterable<E> implements Iterable<E>
@@ -26,6 +27,13 @@ public class SuperIterable<E> implements Iterable<E>
         }
 
         return new SuperIterable<E>(list);
+    }
+
+    private void forEvery(Consumer<E> cons) {
+        for (E e : self) {
+            cons.accept(e);
+        }
+
     }
     @Override
     public Iterator<E> iterator()
